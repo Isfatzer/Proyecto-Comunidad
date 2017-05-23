5f1f27377250db71aec09b42afefa377484ea603
isrom <isrom@DESKTOP-0R4GDFE>
2017-05-22 23:19:08 -0600

    Declaracion de clases

---
+++ Proyecto-Comunidad/Proyecto-Comunidad/Gastos.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Comunidad
+{
+    class Gastos
+    {
+        string identificacion;
+        string nombre;
+        string tipo_de_Reparto;
+
+        public string Identificacion
+        {
+            get
+            {
+                return identificacion;
+            }
+
+            set
+            {
+                identificacion = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string Tipo_de_Reparto
+        {
+            get
+            {
+                return tipo_de_Reparto;
+            }
+
+            set
+            {
+                tipo_de_Reparto = value;
+            }
+        }
+    }
+}
---
+++ Proyecto-Comunidad/Proyecto-Comunidad/Propietarios.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Comunidad
+{
+    class Propietarios
+    {
+        string nombre;
+        int nit;
+        string email;
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public int Nit
+        {
+            get
+            {
+                return nit;
+            }
+
+            set
+            {
+                nit = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+    }
+}
